58ad481b2a97bbf4db6cb402414828c3b2069676
Innoper <teaminnoper@gamil.com>
2024-12-02 03:48:40 +0900

    Feat: jtrc Button 컴포넌트 isLoading props 추가

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
+  height?: string | number;
   variant?: "primary" | "secondary" | "default";
+  isLoading?: boolean;
 }
+
+const bounce = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px); /* 점이 올라가는 높이 */
+  }
+`;
 
 const defaultColor = {
   primary: {
@@ -26,7 +37,7 @@ const defaultColor = {
 
 const StyledButton = styled.button<ButtonProps>`
   width: 100%;
-  height: auto;
+  height: ${(props) => props.height || "3rem"};
   white-space: normal; /* 줄바꿈 허용 */
   word-break: break-word; /* 긴 단어를 줄바꿈 */
   display: flex;
@@ -34,7 +45,7 @@ const StyledButton = styled.button<ButtonProps>`
   justify-content: center;
   gap: 0.5rem;
   padding: 0.7em 1em;
-  font-size: 1rem;
+  font-size: 1.2rem;
   line-height: 1;
   border-radius: 0.5rem;
   cursor: pointer;
@@ -56,12 +67,46 @@ const StyledButton = styled.button<ButtonProps>`
       props.variant !== "default"
         ? `var(--jtrc-button-color, var(--jtrc-thema, ${defaultColor.primary.borderColor}))`
         : ` ${defaultColor.default.borderColor}`};
+
+  &:disabled {
+    opacity: 0.7;
+  }
 `;
 
-const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+// 로딩 컨테이너
+const LoadingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px; /* 점들 간의 간격 */
+`;
+
+// 점 스타일
+const Dot = styled.div<{ delay: string }>`
+  width: 8px;
+  height: 8px;
+  background-color: #777777; /* 원하는 색상 */
+  border-radius: 50%;
+  animation: ${bounce} 1s infinite ease-in-out;
+  animation-delay: ${(props) => props.delay};
+`;
+
+const Button: React.FC<ButtonProps> = ({ children, isLoading, ...props }) => {
   return (
-    <StyledButton variant={props.variant || "primary"} {...props}>
-      {children}
+    <StyledButton
+      disabled={isLoading}
+      variant={props.variant || "primary"}
+      {...props}
+    >
+      {!isLoading ? (
+        children
+      ) : (
+        <LoadingContainer>
+          <Dot delay="0s" />
+          <Dot delay="0.2s" />
+          <Dot delay="0.4s" />
+        </LoadingContainer>
+      )}
     </StyledButton>
   );
 };
